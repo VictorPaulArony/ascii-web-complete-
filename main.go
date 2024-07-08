d61363abc3fd451cd7689f8f8547289f12916cec
victorpaularony <victorpualarony@gmail.com>
2024-07-08 14:17:46 +0300

    FILE: main.go file for the asci-web complete program

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strings"
+
+	"ascii-web-complete/ascii"
+)
+
+func main() {
+	http.HandleFunc("/", Handler)
+	fmt.Println("Server open on http://localhost:8080")
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "ERROR PARSING THE FORM", http.StatusBadRequest)
+		return
+	}
+
+	type Data struct {
+		Result string
+		Color  string
+	}
+
+	var result strings.Builder
+	word := r.Form.Get("Word")
+	files := r.Form.Get("Files")
+	color := r.Form.Get("Colors")
+
+	if strings.Contains(word, "\n") {
+		slice := strings.Split(word, "\r\n")
+		for _, char := range slice {
+			result.WriteString(Inputs([]string{char, files}))
+		}
+	} else {
+		result.WriteString(Inputs([]string{word, files}))
+	}
+
+	data := Data{
+		Result: result.String(),
+		Color:  color,
+	}
+
+	temp, err := template.ParseFiles("index.html")
+	if err != nil {
+		http.Error(w, "ERROR WHILE PARSING THE FILES", http.StatusInternalServerError)
+		return
+	}
+
+	err = temp.Execute(w, data)
+	if err != nil {
+		http.Error(w, "ERROR EXECUTING THE PROGRAM", http.StatusInternalServerError)
+	}
+}
+
+func Inputs(input []string) string {
+	inputs := strings.ReplaceAll(input[0], "\\n", "\n")
+	filenames := input[1]
+	return ascii.Ascii(inputs, filenames)
+}
